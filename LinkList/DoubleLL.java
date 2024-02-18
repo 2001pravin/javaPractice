c9860ad400d9adbeb8493e30016610f18ef57d2b
pravin pralhad patil <113687095+2001pravin@users.noreply.github.com>
2024-02-18 15:02:59 +0530

    hii

--- LinkList/DoubleLL.java
+++ LinkList/DoubleLL.java
@@ -29,6 +29,55 @@ public class DoubleLL {
          head = newNode;
     }
     // add last
+    public void Addlast(int data){
+      Node newNode= new Node(data);
+      size++;
+      if (head==null){
+        head= tail=newNode;
+        return;
+     }
+      newNode.prev=tail;
+      tail.next=newNode;
+      tail=newNode;
+      
+
+    }
+    public int removeFirst(){
+        if (head==null){
+            System.out.println("dl is empty");
+            return Integer.MAX_VALUE;
+         }
+         if (size==1) {
+            int val=head.data;
+            head= tail= null;
+            size--;
+            return val;
+
+         }
+         int val=head.data;
+         head=head.next;
+         head.prev=null;
+         size--;
+         return val;
+    }
+    
+    public int removeLast(){
+        if (head==null){
+            System.out.println("dl is empty");
+            return Integer.MAX_VALUE;
+         }
+         if (size==1) {
+            int val=head.data;
+            head= tail= null;
+            size--;
+            return val;
+         }
+        int val= tail.data;
+        tail=tail.prev;
+        tail.next=null;
+        size--;
+        return val;
+    }
 
 
     public void print(){
@@ -39,6 +88,21 @@ public class DoubleLL {
         }
         System.out.println("null");
     }
+    public  void revers() {
+        Node curr =head;
+        Node prev=null;
+        Node next;
+
+        while(curr!=null){
+          next= curr.next;
+          curr.next= prev;
+          curr.prev=next;
+          prev=curr;
+          curr=next;
+        }
+        head=prev;
+
+    }
      public static void main(String[] args) {
         DoubleLL dl=new DoubleLL();
         dl.AddFirst(5);
@@ -46,6 +110,14 @@ public class DoubleLL {
         dl.AddFirst(8);
         dl.AddFirst(9);
         dl.AddFirst(10);
+        dl.Addlast(12);
+       System.out.println(dl.size); 
+       dl.print();
+        dl.removeFirst();
+        dl.removeLast();
+        System.out.println(dl.size); 
+        dl.print();
+        dl.revers();
         dl.print();
      }
 
